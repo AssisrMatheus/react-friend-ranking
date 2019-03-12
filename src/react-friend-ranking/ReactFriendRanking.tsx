ffc31f1ba329c9768191365cac426a0b117cc950
AdowTatep <assisr.matheus@gmail.com>
2019-03-12 17:42:28 -0300

    :sparkles: Adding a parent component

---
+++ src/react-friend-ranking/ReactFriendRanking.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class ReactFriendRanking extends React.Component {
+    public render() {
+        return (
+            <p>Hello world - React friend ranking</p>
+        );
+    }
+}
+
+export default ReactFriendRanking;
