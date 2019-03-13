9678bdcc7524829ba3422b45dd96bed6dd0315fc
AdowTatep <assisr.matheus@gmail.com>
2019-03-13 11:15:06 -0300

    Updates

---
+++ precache-manifest.271d7492941fb3f6470fc1f50ebc672c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "3cf97837524dd7445e9d1462e3c4afe2",
+    "url": "/react-friend-ranking/static/media/open-iconic.3cf97837.woff"
+  },
+  {
+    "revision": "d77929c85cc19c654feb",
+    "url": "/react-friend-ranking/static/css/main.80538773.chunk.css"
+  },
+  {
+    "revision": "2fc63abcd8da1d2518ee",
+    "url": "/react-friend-ranking/static/js/runtime~main.476458cf.js"
+  },
+  {
+    "revision": "eca2c26759a009a4a9361151efe99e74",
+    "url": "/react-friend-ranking/static/media/open-iconic.eca2c267.svg"
+  },
+  {
+    "revision": "f7d1474133fb1e828bb9",
+    "url": "/react-friend-ranking/static/js/2.0812cd02.chunk.js"
+  },
+  {
+    "revision": "d77929c85cc19c654feb",
+    "url": "/react-friend-ranking/static/js/main.e04a3818.chunk.js"
+  },
+  {
+    "revision": "527eff645d8ead16666afb6931041279",
+    "url": "/react-friend-ranking/static/media/open-iconic.527eff64.otf"
+  },
+  {
+    "revision": "47151e87c5a8666791a91007de4962da",
+    "url": "/react-friend-ranking/static/media/open-iconic.47151e87.eot"
+  },
+  {
+    "revision": "93cc7aa654ae36b0828716f5ec3159cd",
+    "url": "/react-friend-ranking/static/media/open-iconic.93cc7aa6.ttf"
+  },
+  {
+    "revision": "f7d1474133fb1e828bb9",
+    "url": "/react-friend-ranking/static/css/2.afd7336c.chunk.css"
+  },
+  {
+    "revision": "86d3cfece1312248fa2460372f7f9c78",
+    "url": "/react-friend-ranking/index.html"
+  }
+];
